875358f16f3e34621b05b54b7a38f6848b55c969
Nahan Trindade <77818587+NahanTrindade@users.noreply.github.com>
2022-12-06 10:45:20 -0400

    feat: Adiciona tela de cadastro de professor

--- src/screens/registerProfessor/components/FormRegister/index.tsx
+++ src/screens/registerProfessor/components/FormRegister/index.tsx
@@ -126,18 +126,6 @@ const FormRegister = () => {
                   name="password"
                   onChange={handlePasswordChange}
                   placeholder="Crie sua senha*"
-                  endAdornment={
-                    <InputAdornment position="end">
-                      <IconButton
-                        aria-label="toggle password visibility"
-                        onClick={handleClickShowPassword}
-                        onMouseDown={handleMouseDownPassword}
-                        edge="end"
-                      >
-                        {showPassword ? <VisibilityOff /> : <Visibility />}
-                      </IconButton>
-                    </InputAdornment>
-                  }
                 />
                 <PasswordError />
               </TextFieldContainer>
