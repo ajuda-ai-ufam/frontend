2007448199275401d9ab97a04749d4fe0ed8dce8
Nahan Trindade <77818587+NahanTrindade@users.noreply.github.com>
2023-03-03 11:54:06 -0400

    fix expired token

--- src/service/api/index.ts
+++ src/service/api/index.ts
@@ -1,5 +1,7 @@
 import axios, { AxiosRequestConfig } from 'axios';
 import useGetToken from '../storage/getToken';
+import useClearStorage from '../storage/clearStorage';
+import { SCREENS } from '../../utils/screens';
 
 const api = axios.create({
   baseURL: process.env.REACT_APP_API_BASE_URL,
@@ -19,5 +21,16 @@ api.interceptors.request.use((config: AxiosRequestConfig) => {
 
   return config;
 });
+
+api.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    useClearStorage();
+    window.location.href = SCREENS.LOGIN;
+    return Promise.reject(error);
+  }
+);
 
 export default api;
