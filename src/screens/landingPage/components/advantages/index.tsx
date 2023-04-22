2cbfb207e3200b349204161796431db056393e19
Marcoszz <marcos.mpdcl@gmail.com>
2023-04-22 13:16:07 -0400

    feat: Adiciona assets

--- src/screens/landingPage/components/advantages/index.tsx
+++ src/screens/landingPage/components/advantages/index.tsx
@@ -6,47 +6,60 @@ import {
 } from '../../styles';
 import {
   BookImage,
+  BottomLeftImage,
+  BottomRightImage,
   BoxContainer,
   HandsImage,
   InfoBox,
   InfoBoxTypography,
   InnerContainer,
   NotificationImage,
+  OutsideContainer,
+  TopLeftImage,
+  TopRightImage,
 } from './styles';
 
 const Advantages = () => {
   return (
-    <SectionContainer color={theme.palette.secondary.main}>
-      <InnerContainer>
-        <TitleTypography color={'white'}>Conheça as vantagens</TitleTypography>
-        <InfoTypography sx={{ margin: '16px 0' }}>
-          Veja abaixo como você pode se beneficiar da nossa plataforma.
-        </InfoTypography>
-        <BoxContainer>
-          <InfoBox>
-            <BookImage />
-            <InfoBoxTypography variant="body1" color={'white'}>
-              Melhore seu desempenho com nossa plataforma ao tirar dúvidas e
-              receber feedback dos monitores.
-            </InfoBoxTypography>
-          </InfoBox>
-          <InfoBox>
-            <NotificationImage />
-            <InfoBoxTypography variant="body1" color={'white'}>
-              Receba notificações e lembretes para sessões de monitoria
-              agendadas para que você nunca perca uma sessão.
-            </InfoBoxTypography>
-          </InfoBox>
-          <InfoBox>
-            <HandsImage />
-            <InfoBoxTypography variant="body1" color={'white'}>
-              Seja um monitor e ajude outros estudantes a alcançar seu potencial
-              acadêmico.
-            </InfoBoxTypography>
-          </InfoBox>
-        </BoxContainer>
-      </InnerContainer>
-    </SectionContainer>
+    <OutsideContainer>
+      <TopLeftImage />
+      <TopRightImage />
+      <BottomLeftImage />
+      <BottomRightImage />
+      <SectionContainer color={theme.palette.secondary.main}>
+        <InnerContainer>
+          <TitleTypography color={'white'}>
+            Conheça as vantagens
+          </TitleTypography>
+          <InfoTypography sx={{ margin: '16px 0' }}>
+            Veja abaixo como você pode se beneficiar da nossa plataforma.
+          </InfoTypography>
+          <BoxContainer>
+            <InfoBox>
+              <BookImage />
+              <InfoBoxTypography variant="body1" color={'white'}>
+                Melhore seu desempenho com nossa plataforma ao tirar dúvidas e
+                receber feedback dos monitores.
+              </InfoBoxTypography>
+            </InfoBox>
+            <InfoBox>
+              <NotificationImage />
+              <InfoBoxTypography variant="body1" color={'white'}>
+                Receba notificações e lembretes para sessões de monitoria
+                agendadas para que você nunca perca uma sessão.
+              </InfoBoxTypography>
+            </InfoBox>
+            <InfoBox>
+              <HandsImage />
+              <InfoBoxTypography variant="body1" color={'white'}>
+                Seja um monitor e ajude outros estudantes a alcançar seu
+                potencial acadêmico.
+              </InfoBoxTypography>
+            </InfoBox>
+          </BoxContainer>
+        </InnerContainer>
+      </SectionContainer>
+    </OutsideContainer>
   );
 };
 
--- src/screens/landingPage/components/advantages/styles.ts
+++ src/screens/landingPage/components/advantages/styles.ts
@@ -10,6 +10,28 @@ import Hands160 from '../../../../assets/landingPageImages/hands160.svg';
 import Notification from '../../../../assets/landingPageImages/notification.svg';
 import Notification100 from '../../../../assets/landingPageImages/notification100.svg';
 import Notification160 from '../../../../assets/landingPageImages/notification160.svg';
+import TopLeftImage1920 from '../../../../assets/landingPageImages/1920/image-41.png';
+import TopLeftImage905 from '../../../../assets/landingPageImages/905/image-41.png';
+import TopLeftImage360 from '../../../../assets/landingPageImages/360/image-41.png';
+import TopRightImage1920 from '../../../../assets/landingPageImages/1920/image-40.png';
+import TopRightImage905 from '../../../../assets/landingPageImages/905/image-40.png';
+import TopRightImage360 from '../../../../assets/landingPageImages/360/image-40.png';
+import BottomLeftImage1920 from '../../../../assets/landingPageImages/1920/image-43.png';
+import BottomLeftImage905 from '../../../../assets/landingPageImages/905/image-43.png';
+import BottomLeftImage360 from '../../../../assets/landingPageImages/360/image-43.png';
+import BottomRightImage1920 from '../../../../assets/landingPageImages/1920/image-42.png';
+import BottomRightImage905 from '../../../../assets/landingPageImages/905/image-42.png';
+import BottomRightImage360 from '../../../../assets/landingPageImages/360/image-42.png';
+
+export const OutsideContainer = styled(Box).attrs({
+  width: '100%',
+  height: '100%',
+  position: 'relative',
+})`
+  @media (max-height: 700px) {
+    margin: 84px 0 0 0 !important;
+  }
+`;
 
 export const InnerContainer = styled(Box).attrs({
   width: '100%',
@@ -20,6 +42,7 @@ export const InnerContainer = styled(Box).attrs({
   alignItems: 'center',
   textAlign: 'center',
   padding: '64px 0',
+  position: 'relative',
 })``;
 
 export const BoxContainer = styled(Box).attrs({
@@ -124,5 +147,93 @@ export const HandsImage = styled(Box)`
     background-image: url(${Hands100}) !important;
     width: 100px !important;
     height: 100px !important;
+  }
+`;
+
+export const TopLeftImage = styled(Box)`
+  position: absolute;
+  top: 0;
+  left: 0;
+  margin-top: -228px;
+  background-image: url(${TopLeftImage1920});
+  width: 298px;
+  height: 388px;
+  @media (max-width: ${theme.breakpoints.values.md}px) {
+    background-image: url(${TopLeftImage905}) !important;
+    margin-top: -120px !important;
+    width: 158px !important;
+    height: 206px !important;
+  }
+  @media (max-width: ${theme.breakpoints.values.sm}px) {
+    background-image: url(${TopLeftImage360}) !important;
+    margin-top: -80px !important;
+    width: 106px !important;
+    height: 139px !important;
+  }
+`;
+
+export const TopRightImage = styled(Box)`
+  position: absolute;
+  top: 0;
+  right: 0;
+  margin-top: -228px;
+  background-image: url(${TopRightImage1920});
+  width: 310px;
+  height: 388px;
+  @media (max-width: ${theme.breakpoints.values.md}px) {
+    background-image: url(${TopRightImage905}) !important;
+    margin-top: -120px !important;
+    width: 164px !important;
+    height: 206px !important;
+  }
+  @media (max-width: ${theme.breakpoints.values.sm}px) {
+    background-image: url(${TopRightImage360}) !important;
+    margin-top: -80px !important;
+    width: 111px !important;
+    height: 139px !important;
+  }
+`;
+
+export const BottomLeftImage = styled(Box)`
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  margin-bottom: -156px;
+  background-image: url(${BottomLeftImage1920});
+  width: 204px;
+  height: 288px;
+  @media (max-width: ${theme.breakpoints.values.md}px) {
+    background-image: url(${BottomLeftImage905}) !important;
+    width: 108px !important;
+    height: 152px !important;
+    margin-bottom: -82px;
+  }
+  @media (max-width: ${theme.breakpoints.values.sm}px) {
+    background-image: url(${BottomLeftImage360}) !important;
+    width: 72px !important;
+    margin-bottom: -56px;
+    height: 102px !important;
+  }
+`;
+
+export const BottomRightImage = styled(Box)`
+  position: absolute;
+  bottom: 0;
+  right: 0;
+  margin-bottom: -156px;
+  background-image: url(${BottomRightImage1920});
+  width: 298px;
+  height: 288px;
+  @media (max-width: ${theme.breakpoints.values.md}px) {
+    background-image: url(${BottomRightImage905}) !important;
+    width: 158px !important;
+    margin-bottom: -82px;
+    height: 152px !important;
+  }
+  @media (max-width: ${theme.breakpoints.values.sm}px) {
+    background-image: url(${BottomRightImage360}) !important;
+    width: 105px !important;
+    margin-bottom: -56px;
+    height: 102px !important;
   }
 `;
