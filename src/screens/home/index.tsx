dba310a086824c34f385b52350052334e6323730
Thyago Lucas <salc@icomp.ufam.edu.br>
2024-01-11 01:49:48 -0400

    fix: Correção do bug na tela de coordenador

--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -2,6 +2,7 @@ import useGetLoggedUser from '../../service/storage/getLoggedUser';
 import { TypeUserEnum } from '../../utils/constants';
 import ProfessorHome from '../professorHome';
 import StudentHome from '../studentHome';
+import Subjects from '../subjects';
 
 const Home = () => {
   const user = useGetLoggedUser();
@@ -9,6 +10,10 @@ const Home = () => {
   if (user) {
     if (user.type_user_id === TypeUserEnum.PROFESSOR) {
       return <ProfessorHome />;
+    }
+
+    if (user.type_user_id === TypeUserEnum.COORDINATOR) {
+      return <Subjects />;
     }
 
     return <StudentHome />;
